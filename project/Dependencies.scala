0a5c59449d4252c26eba5a64e669f6640b43626e
Gregor Purdy <gregor@abcelo.com>
2023-06-25 17:39:53 -0700

    Upgrade build to support Scala 2.13.11 and 3.3.0

---
+++ project/Dependencies.scala
@@ -0,0 +1,21 @@
+import sbt._
+
+object Dependencies {
+  val LogbackVersion = "1.4.8"
+  lazy val Logback = "ch.qos.logback" % "logback-classic" % LogbackVersion
+
+  val Slf4JVersion = "2.0.7"
+  lazy val Slf4JApi = "org.slf4j" % "slf4j-api" % Slf4JVersion
+  lazy val JclOverSlf4J = "org.slf4j" % "jcl-over-slf4j" % Slf4JVersion
+  lazy val Log4JOverSlf4J = "org.slf4j" % "log4j-over-slf4j" % Slf4JVersion
+  lazy val JulToSlf4J = "org.slf4j" % "jul-to-slf4j" % Slf4JVersion
+
+  val ZioHttpVersion = "3.0.0-RC2"
+  lazy val ZioHttp = "dev.zio" %% "zio-http" % ZioHttpVersion
+
+  val ZioVersion = "2.0.15"
+  lazy val Zio = "dev.zio" %% "zio" % ZioVersion
+  lazy val ZioTest = "dev.zio" %% "zio-test" % ZioVersion
+  lazy val ZioTestMagnolia = "dev.zio" %% "zio-test-magnolia" % ZioVersion
+  lazy val ZioTestSbt = "dev.zio" %% "zio-test-sbt" % ZioVersion
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,4 @@
+addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.7.0")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.0")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.11.0")
+addDependencyTreePlugin
