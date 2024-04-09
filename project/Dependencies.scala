1e1f4dcc8c51c33c2eea7075f0ff80f53ea3e2c0
Scala Steward <scala_steward@virtuslab.com>
2024-04-09 15:29:07 +0000

    Update logback-classic to 1.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val LogbackVersion = "1.5.3"
+  val LogbackVersion = "1.5.4"
   lazy val Logback = "ch.qos.logback" % "logback-classic" % LogbackVersion
 
   val Slf4JVersion = "2.0.12"
